237fd85c22fc0d62beeb45c7fec87f53cb3ba023
luxejs <luxe.jsx@gmail.com>
2024-03-01 22:55:48 -0700

    init

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const app = express()
+
+const HTTP_PORT = 8080
+const path = require("path")
+
+app.use(express.static("public"))
+app.set('view engine', 'ejs');
+
+
+app.get("/", (req, res) => {
+    res.render('index2')
+})
+
+
+app.use((req, res, next) => {
+    res.status(404).send("404 - We're unable to find what you're looking for.");
+})
+
+app.listen(HTTP_PORT, () => {
+    console.log(`server listening at port ${HTTP_PORT}`)
+})
